eb08f5a48db7a548ad21fadc1372e9cb82de2883
Sun.k <lovesunsixin@qq.com>
2020-03-14 18:02:01 +0800

    文件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 日志的详细与否 生产环境 开发环境
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+// main.js 入口文件
+// 职责：做全局范围内的配置和依赖  创建一个根VUE实例
